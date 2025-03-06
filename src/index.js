e5820119b504424bae9881537394ecc2e0c8fb11
Dmitriy Lyubenko <lobedima@gmail.com>
2025-03-06 12:24:05 +0300

    add change

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import fastify from 'fastify';
 const app = fastify();
 const port = 3000;
 
+app.get('/hello', async (request, reply) => {
+  const name = request.query.name || 'World';
+  return `Hello, ${name}!`;
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+  res.send('GET /users');
+});
+
+app.post('/users', (req, res) => {
+  res.send('POST /users');
 });
 
 app.listen({ port }, () => {
